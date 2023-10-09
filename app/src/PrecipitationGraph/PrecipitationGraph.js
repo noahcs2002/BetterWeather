a2b2bd1ed21da4ec7d20354f883b91cd87c431bf
Noah Sternberg <noahcs2002@gmail.com>
2023-10-09 12:59:04 -0500

    Implement rudimentary precip graph

---
+++ app/src/PrecipitationGraph/PrecipitationGraph.js
@@ -0,0 +1,33 @@
+import React from "react";
+import './PrecipitationGraph.scss';
+import Visualisation from "../Vis/Visualisation";
+
+export default function Precipitation({data}) {
+
+
+    const xData=data.daily.xData;
+    const yHighs=[]
+    var yLows=[]
+    const title='Precipitation Chances';
+    const xAxis='Day';
+    const yAxis='Rain Chance';
+
+    data.weeklyPeriods.forEach(period => {
+        if (!period.probabilityOfPrecipitation.value) {
+            yLows.push(0);
+        }
+        else {
+            yLows.push(period.probabilityOfPrecipitation.value);
+        }
+    })
+
+
+    console.log('Periods recieved: ', data.weeklyPeriods);
+
+    return (
+        <div className="precip">
+            <h2> Precipitation Chances: </h2>
+            <Visualisation xData={xData} yHighs = {[]} yLows={yLows} title={title} xAxis={xAxis} yAxis={yAxis} type='s'/>
+        </div>
+    )
+}
