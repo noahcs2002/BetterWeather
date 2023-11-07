7171ab6acf497d504cba37d996be8289e3aa4b87
Noah Sternberg <nsternberg@murraystate.edu>
2023-11-06 22:30:38 -0600

    Update FAQ's

--- app/src/Help/Help.js
+++ app/src/Help/Help.js
@@ -32,26 +32,15 @@ const Help = () => {
             id : 2
         },
         {
-            q : 'Why are the numbers I\'m seeingly slightly different than ____\'s app ?', 
-            a : 'The data we get is from the National Weather Service, and numbers might vary by a degree or two.',
-            id : 3
-        },
-        {
             q : 'Where are you getting this information?', 
             a : 'The National Weather Service\'s free data.',
-            id : 4
+            id : 3
         },
         {
             q: 'Why does the site not have data for ____ ?',
             a: 'Our data comes from the National Weather Service. If the data is missing for your city or area, ' +
-            ' then we recommend trying to search for a different area, or include a state in the search field.',
-            id : 5
-        },
-        {
-            q: 'How can I get in touch?',
-            a: 'If you need to get in contact with us, you can use the Google Forms provided in the Feedback tab, or send us an email' +
-            ' at betterweather.contact@gmail.com. We will try and respond within 48 buisness hours.',
-            id : 6
+            ' then we recommend trying to search for a different area nearby to your desired area, or include a state in the search field for a more refined search.',
+            id : 4
         }
     ]
 
